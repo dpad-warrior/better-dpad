6b067e4df76817eebd6a658d7910545f4dd66501
dpad-warrior <davidchai717@gmail.com>
2026-02-25 11:30:54 -0500

    feat: added ability to override behaviors for specific apps

--- app/src/main/java/com/dpadwarrior/betterdpad/accessibility/appconfigs/GoogleMapsConfig.kt
+++ app/src/main/java/com/dpadwarrior/betterdpad/accessibility/appconfigs/GoogleMapsConfig.kt
@@ -15,50 +15,58 @@ class GoogleMapsConfig : AppAccessibilityConfig() {
 
     override val fabLabel = "Re-center map to your location"
 
-    fun detectScreen(rootNode: AccessibilityNodeInfo): Screen {
-        if (hasNodeWithText(rootNode, "Search here")) return Screen.MAIN_SCREEN
-        return Screen.UNKNOWN
+    override fun findFirstFocusable(rootNode: AccessibilityNodeInfo): AccessibilityNodeInfo? {
+        return rootNode.findAccessibilityNodeInfosByText("Search here").firstOrNull()
     }
 
-    override fun onKeyEvent(
-        event: KeyEvent,
-        rootNode: AccessibilityNodeInfo?
-    ): Boolean {
-        if (rootNode == null) return false
-
-        if (event.action == KeyEvent.ACTION_DOWN) {
-            when (detectScreen(rootNode)) {
-                Screen.MAIN_SCREEN -> {
-                    when (event.keyCode) {
-                        KeyEvent.KEYCODE_0 -> {
-                            val nodes = rootNode.findAccessibilityNodeInfosByText("Search here")
-                            nodes.firstOrNull()?.performAction(AccessibilityNodeInfo.ACTION_FOCUS)
-                            nodes.forEach { it.recycle() }
-                            return true
-                        }
-                        KeyEvent.KEYCODE_POUND -> {
-                            var nodes = rootNode.findAccessibilityNodeInfosByText("Re-center map to your location")
-                            if (nodes.isEmpty()) {
-                                nodes = rootNode.findAccessibilityNodeInfosByText("Enter compass mode")
-                            }
-                            nodes.firstOrNull()?.performAction(AccessibilityNodeInfo.ACTION_FOCUS)
-                            nodes.forEach { it.recycle() }
-                            return true
-                        }
-                    }
-                }
-                Screen.UNKNOWN -> {
-                }
-            }
-        }
-        return false
+    override fun findLastFocusable(rootNode: AccessibilityNodeInfo): AccessibilityNodeInfo? {
+        return rootNode.findAccessibilityNodeInfosByText("Contribute").firstOrNull()
     }
 
-    private fun hasNodeWithText(rootNode: AccessibilityNodeInfo, text: String): Boolean {
-        val nodes = rootNode.findAccessibilityNodeInfosByText(text)
-        Log.d("BetterDpad", "Nodes found with text ${text}: ${nodes.size}")
-        val found = nodes.isNotEmpty()
-        nodes.forEach { it.recycle() }
-        return found
-    }
+//    fun detectScreen(rootNode: AccessibilityNodeInfo): Screen {
+//        if (hasNodeWithText(rootNode, "Search here")) return Screen.MAIN_SCREEN
+//        return Screen.UNKNOWN
+//    }
+
+//    override fun onKeyEvent(
+//        event: KeyEvent,
+//        rootNode: AccessibilityNodeInfo?
+//    ): Boolean {
+//        if (rootNode == null) return false
+//
+//        if (event.action == KeyEvent.ACTION_DOWN) {
+//            when (detectScreen(rootNode)) {
+//                Screen.MAIN_SCREEN -> {
+//                    when (event.keyCode) {
+//                        KeyEvent.KEYCODE_0 -> {
+//                            val nodes = rootNode.findAccessibilityNodeInfosByText("Search here")
+//                            nodes.firstOrNull()?.performAction(AccessibilityNodeInfo.ACTION_FOCUS)
+//                            nodes.forEach { it.recycle() }
+//                            return true
+//                        }
+//                        KeyEvent.KEYCODE_POUND -> {
+//                            var nodes = rootNode.findAccessibilityNodeInfosByText("Re-center map to your location")
+//                            if (nodes.isEmpty()) {
+//                                nodes = rootNode.findAccessibilityNodeInfosByText("Enter compass mode")
+//                            }
+//                            nodes.firstOrNull()?.performAction(AccessibilityNodeInfo.ACTION_FOCUS)
+//                            nodes.forEach { it.recycle() }
+//                            return true
+//                        }
+//                    }
+//                }
+//                Screen.UNKNOWN -> {
+//                }
+//            }
+//        }
+//        return false
+//    }
+
+//    private fun hasNodeWithText(rootNode: AccessibilityNodeInfo, text: String): Boolean {
+//        val nodes = rootNode.findAccessibilityNodeInfosByText(text)
+//        Log.d("BetterDpad", "Nodes found with text ${text}: ${nodes.size}")
+//        val found = nodes.isNotEmpty()
+//        nodes.forEach { it.recycle() }
+//        return found
+//    }
 }
